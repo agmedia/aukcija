3c9e8d2b2941249957a117f617f7fd9b320246d9
Filip Jankoski <filip@agmedia.hr>
2025-03-06 12:48:11 +0100

    _fix: Auction seeder.

--- database/factories/Back/Catalog/Auction/AuctionImageFactory.php
+++ database/factories/Back/Catalog/Auction/AuctionImageFactory.php
@@ -20,7 +20,7 @@ class AuctionImageFactory extends Factory
         return [
             'auction_id' => fake()->numberBetween(1, Auction::count()),
             'image' => fake()->imageUrl,
-            'alt' => fake()->words(),
+            'alt' => fake()->words(3, true),
             'default' => 0,
             'published' => 1,
             'sort_order' => 0,
